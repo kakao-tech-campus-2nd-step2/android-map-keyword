89f53cf24a4582f62899c6393d09940a2fd5e407
Lorenzo <tnalsdlekgsk1@icloud.com>
2024-07-01 22:12:46 +0900

    feat(DBHelper): insertDummyData() 함수 구현
    private fun insertDummyData(db: SQLiteDatabase?): 더미 데이터를 삽입하기 위한 함수입니다. onCreate()안에 호출하여 한 번만 삽입하도록 했습니다.

--- app/src/main/java/campus/tech/kakao/map/DBHelper.kt
+++ app/src/main/java/campus/tech/kakao/map/DBHelper.kt
@@ -13,6 +13,7 @@ class DBHelper (context: Context): SQLiteOpenHelper(context, "place.db", null, 1
             "${PlaceContract.TABLE_COLUMN_CATEGORY} VARCHAR(30) NOT NULL"+
             ");"
         )
+        insertDummyData(db)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
@@ -20,7 +21,7 @@ class DBHelper (context: Context): SQLiteOpenHelper(context, "place.db", null, 1
         onCreate(db)
     }
 
-    fun insert(db: SQLiteDatabase, place: Place{
+    fun insert(db: SQLiteDatabase, place: Place) {
         val sql = " INSERT INTO " +
                 "${PlaceContract.TABLE_NAME}("+
                 "${PlaceContract.TABLE_COLUMN_NAME}, ${PlaceContract.TABLE_COLUMN_ADDRESS}, ${PlaceContract.TABLE_COLUMN_CATEGORY})"+
@@ -32,4 +33,23 @@ class DBHelper (context: Context): SQLiteOpenHelper(context, "place.db", null, 1
     fun select(db: SQLiteDatabase,name:String, address: String, category: String) : String? {
         return null
     }
+
+    private fun insertDummyData(db: SQLiteDatabase?) {
+        db?.let {
+            val categories = listOf("카페", "약국", "식당")
+            val baseAddress = "서울 성동구 성수동"
+
+            categories.forEach { category ->
+                for (i in 1..15) {
+                    val name = "$category $i"
+                    val address = "$baseAddress $i"
+                    val sql = "INSERT INTO ${PlaceContract.TABLE_NAME} (" +
+                            "${PlaceContract.TABLE_COLUMN_NAME}, ${PlaceContract.TABLE_COLUMN_ADDRESS}, ${PlaceContract.TABLE_COLUMN_CATEGORY}) " +
+                            "VALUES ('$name', '$address', '$category');"
+                    it.execSQL(sql)
+                }
+            }
+        }
+    }
+
 }
