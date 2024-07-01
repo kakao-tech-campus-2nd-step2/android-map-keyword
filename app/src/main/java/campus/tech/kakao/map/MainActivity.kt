7f643202570e5f544c4bb75ec8cb315d86c8c01b
LEEMINSEO <real2ms@naver.com>
2024-07-02 02:15:18 +0900

    feat: clear button

--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -1,11 +1,30 @@
 package campus.tech.kakao.map
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var inputSearch: EditText
+    private lateinit var buttonX: Button
+    private lateinit var noResult: TextView
+    private lateinit var searchListView: ListView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        inputSearch = findViewById(R.id.inputSearch)
+        buttonX = findViewById(R.id.buttonX)
+        noResult = findViewById(R.id.noResult)
+        searchListView = findViewById(R.id.searchListView)
+
+        buttonX.setOnClickListener {
+            inputSearch.text.clear()
+        }
     }
 }
