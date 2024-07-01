6aaaa3b77051b095fe66ae11778169e2db4cb4e4
KSC <ksc1008@naver.com>
2024-07-01 14:07:34 +0900

    layout: setup base layout (mainActivity, SearchResultFragment)

--- app/src/main/java/campus/tech/kakao/map/Views/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/Views/MainActivity.kt
@@ -1,12 +1,26 @@
 package campus.tech.kakao.map.Views
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentContainerView
 import campus.tech.kakao.map.R
 
 class MainActivity : AppCompatActivity() {
+    lateinit var searchResultFragmentContainer: FragmentContainerView
+    lateinit var searchInput: EditText
+    lateinit var savedWordBar: View
+
+    private fun initiateViews(){
+        searchResultFragmentContainer = findViewById(R.id.fragment_container_search_result)
+        searchInput = findViewById(R.id.input_search)
+        savedWordBar = findViewById(R.id.saved_search_bar)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initiateViews()
     }
 }
