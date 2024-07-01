5c26a9c7261ba6d130fe46a9263beb5e757823ca
leeghy <epuy109@gmail.com>
2024-07-01 15:43:31 +0900

    dbHelper 클래스 채우기

--- app/src/main/java/campus/tech/kakao/map/DataDbHelper.kt
+++ app/src/main/java/campus/tech/kakao/map/DataDbHelper.kt
@@ -6,10 +6,16 @@ import android.database.sqlite.SQLiteOpenHelper
 
 class DataDbHelper(context: Context) : SQLiteOpenHelper(context, "data.db", null, 1) {
     override fun onCreate(db: SQLiteDatabase?) {
-        
+        val sql: String = "CREATE TABLE if not exists locations(" +
+                "name String," +
+                "location String," +
+                "category String);"
+        db.execSQL(sql)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-
+        val sql: String = "DROP TABLE if exists locations"
+        db?.execSQL(sql)
+        onCreate(db)
     }
 }
