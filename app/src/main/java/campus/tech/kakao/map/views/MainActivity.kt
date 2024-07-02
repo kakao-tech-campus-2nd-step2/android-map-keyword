a1f1dc264b924090189f407011b68edaa8159dda
KSC <ksc1008@naver.com>
2024-07-02 14:37:51 +0900

    feat: add method to print all data from database

--- app/src/main/java/campus/tech/kakao/map/views/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/views/MainActivity.kt
@@ -1,18 +1,21 @@
 package campus.tech.kakao.map.views
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentContainerView
 import campus.tech.kakao.map.R
+import campus.tech.kakao.map.models.SearchDbHelper
+import campus.tech.kakao.map.models.SearchResult
 
 class MainActivity : AppCompatActivity() {
     lateinit var searchResultFragmentContainer: FragmentContainerView
     lateinit var searchInput: EditText
     lateinit var savedWordBar: View
 
-    private fun initiateViews(){
+    private fun initiateViews() {
         searchResultFragmentContainer = findViewById(R.id.fragment_container_search_result)
         searchInput = findViewById(R.id.input_search)
         savedWordBar = findViewById(R.id.saved_search_bar)
@@ -22,5 +25,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initiateViews()
+
+        getDataFromDb()
+    }
+
+    private fun getDataFromDb() {
+        val data = SearchDbHelper.getInstance(applicationContext).queryAll()
+        printDebugLog(data)
+    }
+
+    private fun printDebugLog(searchResults: List<SearchResult>) {
+        val stringBuilder: StringBuilder = StringBuilder()
+        for ((i, data) in searchResults.withIndex()) {
+            stringBuilder.appendLine("[$i] name: ${data.name}, address: ${data.address}, type: ${data.type}")
+        }
+        Log.d("KSC", stringBuilder.toString())
     }
 }
