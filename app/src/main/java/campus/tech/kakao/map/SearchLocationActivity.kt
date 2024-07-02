e53a1bea82951edad851773f4a72692601daa9f7
Jeong Suhyeon <jsh00325@naver.com>
2024-07-02 14:15:11 +0900

    feat: Add Location Search Layout

---
+++ app/src/main/java/campus/tech/kakao/map/SearchLocationActivity.kt
@@ -0,0 +1,15 @@
+package campus.tech.kakao.map
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SearchLocationActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_search_location)
+
+	}
+}
