5b451409b399b05f18937fcc7be0d98a37e809ef
LEEMINSEO <real2ms@naver.com>
2024-07-03 01:45:32 +0900

    feat: update MainActivity

--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var buttonX: Button
     private lateinit var noResult: TextView
     private lateinit var searchListView: ListView
+    private lateinit var dbHelper: DbHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +24,48 @@ class MainActivity : AppCompatActivity() {
         noResult = findViewById(R.id.noResult)
         searchListView = findViewById(R.id.searchListView)
 
+        dbHelper = DbHelper(this)
+
+        insertInitialData(dbHelper)
+
         buttonX.setOnClickListener {
             inputSearch.text.clear()
         }
+    }
+
+    private fun insertInitialData(dbHelper: DbHelper) {
+        for(i in 1..10) {
+            val name = "카페 $i"
+            val address = "서울 성동구 성수동 $i"
+            val category = "카페"
+            dbHelper.insertData(name, address, category)
+        }
+
+        for(i in 1..10) {
+            val name = "약국 $i"
+            val address = "서울 강남구 대치동 $i"
+            val category = "약국"
+            dbHelper.insertData(name, address, category)
+        }
+    }
+
+    private fun searchDatabase(query: String) {
+        val db = dbHelper.readableDatabase
+        val cursor = db.rawQuery(
+            "SELECT * FROM ${PlaceContract.TABLE_NAME} WHERE " +
+                    "${PlaceContract.COLUMN_NAME} LIKE ? OR " +
+                    "${PlaceContract.COLUMN_ADDRESS} LIKE ? OR " +
+                    "${PlaceContract.COLUMN_CATEGORY} LIKE ?",
+            arrayOf("%$query%", "%$query%", "%$query%")
+        )
+
+        val results = mutableListOf<String>()
+        while (cursor.moveToNext()) {
+            val name = cursor.getString(cursor.getColumnIndexOrThrow(PlaceContract.COLUMN_NAME))
+            val address = cursor.getString(cursor.getColumnIndexOrThrow(PlaceContract.COLUMN_ADDRESS))
+            val category = cursor.getString(cursor.getColumnIndexOrThrow(PlaceContract.COLUMN_CATEGORY))
+            results.add("Name: $name, Address: $address, Category: $category")
+        }
+        cursor.close()
     }
 }
