9ee18cf20cefc1008f1423540aa6c3c5583dc137
Jeong hoon <hoondb@naver.com>
2024-07-03 03:04:03 +0900

    refactor: MVVM 패턴 적용
    - MVVM 패턴 적용 시도
    - ViewModel 싱글톤 구현
    
    Remaining issues:
    - 옵저버에서 데이터 베이스의 변화를 1번만 감지하는 문제 발생

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
 
     buildFeatures {
         viewBinding = true
+        dataBinding = true
     }
 }
 
@@ -47,6 +48,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.activity:activity:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/campus/tech/kakao/map/LocationContract.kt
+++ app/src/main/java/campus/tech/kakao/map/LocationContract.kt
@@ -2,8 +2,18 @@ package campus.tech.kakao.map
 
 
 object LocationContract{
+    const val DATABASE_NAME = "location.db"
+
     const val TABLE_NAME: String = "DB_Location"
     const val COLUMN_NAME: String = "name"
     const val COLUMN_LOCATION: String = "location"
     const val COLUMN_TYPE: String = "type"
+
+    const val CREATE_QUERY = "CREATE TABLE ${TABLE_NAME} (" +
+            "${COLUMN_NAME} TEXT NOT NULL, " +
+            "${COLUMN_LOCATION} TEXT NOT NULL, " +
+            "${COLUMN_TYPE} TEXT" +
+            ");"
+
+    const val DROP_QUERY = "DROP TABLE IF EXISTS ${TABLE_NAME}"
 }
