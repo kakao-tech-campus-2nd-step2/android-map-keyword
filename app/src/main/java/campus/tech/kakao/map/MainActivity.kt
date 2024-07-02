c2ba970e8d5be9b9bffeac8ccff81c2452b3b271
anyooin <anyooin123@naver.com>
2024-07-02 22:43:09 +0900

    feat : selectList adapter

--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -20,39 +20,17 @@ class MainActivity : AppCompatActivity() {
             mapItemList.add(MapItem("카페" + i, "복현동" + i, "카페"))
         }
 
+        val selectList = findViewById<RecyclerView>(R.id.selectList)
+        val selectItemList = mutableListOf<SelectItem>()
+        for (i in 0..20) {
+            selectItemList.add(SelectItem("카페" + i))
+        }
+
         mapList.adapter = MapListAdapter(mapItemList, LayoutInflater.from(this))
         mapList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-    }
-}
 
-class MapListAdapter(
-    val mapItemList: MutableList<MapItem>, val layoutInflater: LayoutInflater
-) : RecyclerView.Adapter<MapListAdapter.ViewHolder>() {
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val name: TextView
-        val address: TextView
-        val category: TextView
-
-        init {
-            name = itemView.findViewById<TextView>(R.id.name)
-            address = itemView.findViewById<TextView>(R.id.address)
-            category = itemView.findViewById<TextView>(R.id.category)
-        }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = layoutInflater.inflate(R.layout.map_item, parent, false)
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.name.text = mapItemList.get(position).name
-        holder.address.text = mapItemList.get(position).address
-        holder.category.text = mapItemList.get(position).category
-    }
-
-    override fun getItemCount(): Int {
-        return mapItemList.size
+        selectList.adapter = SelectListAdapter(selectItemList, LayoutInflater.from(this))
+        selectList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
     }
 }
 
--- app/src/main/java/campus/tech/kakao/map/SelectItem.kt
+++ app/src/main/java/campus/tech/kakao/map/SelectItem.kt
@@ -1,4 +1,36 @@
 package campus.tech.kakao.map
 
-class SelectItem {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class SelectItem(
+    val name: String
+)
+
+class SelectListAdapter(
+    val selectItemList: MutableList<SelectItem>, val layoutInflater: LayoutInflater
+) : RecyclerView.Adapter<SelectListAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val name: TextView
+
+        init {
+            name = itemView.findViewById<TextView>(R.id.name)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = layoutInflater.inflate(R.layout.select_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.name.text = selectItemList.get(position).name
+    }
+
+    override fun getItemCount(): Int {
+        return selectItemList.size
+    }
 }
