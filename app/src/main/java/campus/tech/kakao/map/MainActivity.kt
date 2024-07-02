277a755dd71ceeb6b9d4af2649b526f35c9ad68e
anyooin <anyooin123@naver.com>
2024-07-03 00:52:20 +0900

    feat : make map database

--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -1,5 +1,6 @@
 package campus.tech.kakao.map
 
+import android.content.ContentValues
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -15,15 +16,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val mapList = findViewById<RecyclerView>(R.id.mapList)
-        val mapItemList = mutableListOf<MapItem>()
+        val selectList = findViewById<RecyclerView>(R.id.selectList)
+
+        val db = MapDbHelper(context = this)
+        val wDb = db.writableDatabase
+        val rDb = db.readableDatabase
+
         for (i in 0..20) {
-            mapItemList.add(MapItem("카페" + i, "서울 성동구 성수동" + i, "카페"))
+            val values = ContentValues()
+            values.put(Map.TABLE_COLUMN_NAME, "카페" + i)
+            values.put(Map.TABLE_COLUMN_ADDRESS, "서울 성동구 성수동" + i)
+            values.put(Map.TABLE_COLUMN_CATEGORY, "카페")
+
+            wDb.insert(Map.TABLE_NAME, null, values)
         }
 
-        val selectList = findViewById<RecyclerView>(R.id.selectList)
+        val cursor = rDb.rawQuery("Select * from ${Map.TABLE_NAME}", null)
+        val mapItemList = mutableListOf<MapItem>()
+        while (cursor.moveToNext()) {
+            mapItemList.add(
+                MapItem(
+                    cursor.getString(cursor.getColumnIndexOrThrow(Map.TABLE_COLUMN_NAME)),
+                    cursor.getString(cursor.getColumnIndexOrThrow(Map.TABLE_COLUMN_ADDRESS)),
+                    cursor.getString(cursor.getColumnIndexOrThrow(Map.TABLE_COLUMN_CATEGORY))
+                )
+            )
+        }
+
+        cursor.close()
+
+
         val selectItemList = mutableListOf<SelectItem>()
         for (i in 0..20) {
-            selectItemList.add(SelectItem("카페" + i))
+            selectItemList.add(SelectItem("약국" + i))
         }
 
         mapList.adapter = MapListAdapter(mapItemList, LayoutInflater.from(this))
--- app/src/main/java/campus/tech/kakao/map/MapItem.kt
+++ app/src/main/java/campus/tech/kakao/map/MapItem.kt
@@ -1,16 +1,44 @@
 package campus.tech.kakao.map
 
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.provider.BaseColumns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class MapItem(
+data class MapItem(
     val name: String,
     val address: String,
     val category: String
 )
+
+object Map : BaseColumns {
+    const val TABLE_NAME = "map"
+    const val TABLE_COLUMN_NAME = "name"
+    const val TABLE_COLUMN_ADDRESS = "address"
+    const val TABLE_COLUMN_CATEGORY = "category"
+}
+
+class MapDbHelper(context: Context) : SQLiteOpenHelper(context, "map.db", null, 1) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(
+            "CREATE TABLE ${Map.TABLE_NAME} (" +
+                    "${Map.TABLE_COLUMN_NAME} varchar(15) not null," +
+                    "${Map.TABLE_COLUMN_ADDRESS} varchar(30) not null," +
+                    "${Map.TABLE_COLUMN_CATEGORY} varchar(10) not null" +
+                    ");"
+        )
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL("DROP TABLE IF EXISTS ${Map.TABLE_NAME}")
+        onCreate(db)
+    }
+}
 
 class MapListAdapter(
     val mapItemList: MutableList<MapItem>, val layoutInflater: LayoutInflater
