5e758e4c7f0ca4c9f91940e87a3ca31f39ac2a63
이지은 <jieun7056@naver.com>
2024-07-02 23:49:32 +0900

    refactor: add LocationDbAccessor to separate db access logic

--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -11,63 +11,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dbHelper = LocationDbHelper(context=this)
-        addLocationData(dbHelper)
-        readLocationData(dbHelper)
+        val dbAccessor = LocationDbAccessor(context=this)
+        addLocationData(dbAccessor)
+        readLocationData(dbAccessor)
     }
 
-    private fun readLocationData(dbHelper: LocationDbHelper) {
-        val db = dbHelper.readableDatabase
-        val projection = arrayOf(
-            LocationEntry.COLUMN_NAME_TITLE,
-            LocationEntry.COLUMN_NAME_ADDRESS,
-            LocationEntry.COLUMN_NAME_CATEGORY
-        )
-
-        val selection = "${LocationEntry.COLUMN_NAME_CATEGORY} = ?"
-        val selectionArgs = arrayOf("카페")
-
-        val sortOrder = "${LocationEntry.COLUMN_NAME_TITLE} ASC"
-
-        val cursor = db.query(
-            LocationEntry.TABLE_NAME,
-            projection,
-            selection,
-            selectionArgs,
-            null,
-            null,
-            sortOrder
-        )
-        val result = mutableListOf<String>()
-        while (cursor.moveToNext()) {
-            result.add(
-                cursor.getString(
-                    cursor.getColumnIndexOrThrow(LocationEntry.COLUMN_NAME_TITLE)
-                )
-            )
-        }
+    private fun readLocationData(dbAccessor: LocationDbAccessor) {
+        val result = dbAccessor.getLocationByCategory("카페")
         Log.d("jieun", "$result")
-        cursor.close()
     }
 
-    private fun addLocationData(dbHelper: LocationDbHelper) {
-        val db = dbHelper.writableDatabase
-
-        for(i in 1..9) {
-            val values = ContentValues()
-            values.put(LocationEntry.COLUMN_NAME_TITLE, "카페"+i)
-            values.put(LocationEntry.COLUMN_NAME_ADDRESS, "부산 부산진구 전포대로"+i)
-            values.put(LocationEntry.COLUMN_NAME_CATEGORY, "카페")
-
-            db?.insert(LocationEntry.TABLE_NAME, null, values)
+    private fun addLocationData(dbAccessor: LocationDbAccessor) {
+        for (i in 1..9) {
+            dbAccessor.insertLocation("카페$i", "부산 부산진구 전포대로$i", "카페")
         }
-        for(i in 1..9) {
-            val values = ContentValues()
-            values.put(LocationEntry.COLUMN_NAME_TITLE, "음식점"+i)
-            values.put(LocationEntry.COLUMN_NAME_ADDRESS, "부산 부산진구 중앙대로"+i)
-            values.put(LocationEntry.COLUMN_NAME_CATEGORY, "음식점")
-
-            db?.insert(LocationEntry.TABLE_NAME, null, values)
+        for (i in 1..9) {
+            dbAccessor.insertLocation("음식점$i", "부산 부산진구 중앙대로$i", "음식점")
         }
     }
+
 }
