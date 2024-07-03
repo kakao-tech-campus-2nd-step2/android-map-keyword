f9c15802a525716bf31a26243c37ba9b72e0af35
LEEMINSEO <real2ms@naver.com>
2024-07-03 23:50:42 +0900

    feat: use ViewBinding

--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -6,30 +6,45 @@ import android.widget.EditText
 import android.widget.ListView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import campus.tech.kakao.map.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    /* ViewBinding을 사용하기 때문에 주석 처리
     private lateinit var inputSearch: EditText
     private lateinit var buttonX: Button
     private lateinit var noResult: TextView
     private lateinit var searchListView: ListView
+    */
+    private lateinit var binding: ActivityMainBinding
     private lateinit var dbHelper: DbHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /* ViewBinding을 사용하기 때문에 주석 처리
         setContentView(R.layout.activity_main)
 
         inputSearch = findViewById(R.id.inputSearch)
         buttonX = findViewById(R.id.buttonX)
         noResult = findViewById(R.id.noResult)
         searchListView = findViewById(R.id.searchListView)
+        */
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         dbHelper = DbHelper(this)
 
         insertInitialData(dbHelper)
 
+        /* ViewBinding을 사용하기 때문에 주석 처리
         buttonX.setOnClickListener {
             inputSearch.text.clear()
+        }*/
+
+        binding.buttonX.setOnClickListener {
+            binding.inputSearch.text.clear()
         }
     }
 
