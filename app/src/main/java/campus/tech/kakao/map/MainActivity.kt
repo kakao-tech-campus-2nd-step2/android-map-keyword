08f1f376de70b7610a1a77ac71d288f09a0534ab
heonniy <dkgus7535@naver.com>
2024-07-04 11:21:37 +0900

    feat: 검색에 사용될 데이터를 로컬 데이터베이스에 생성

--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -1,11 +1,52 @@
 package campus.tech.kakao.map
 
+import android.content.ContentValues
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import campus.tech.kakao.map.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var DBHelper: DBHelper
+    lateinit var DB: SQLiteDatabase
+    lateinit var adapter: RecycleAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        DBHelper = DBHelper(this, "place.db", null, 2)
+        MakeDummyData("카페")
+        MakeDummyData("약국")
+
+        SetupView()
+    }
+
+    fun MakeDummyData(category: String) {
+        DB = DBHelper.writableDatabase
+        for (i in 1..20) {
+            val place = Place(category + i, "대구 경북대학교로 $i 길", category)
+            insertPlace(place)
+        }
+    }
+
+    fun insertPlace(place: Place) {
+        val values = ContentValues()
+        values.put(PlaceEntry.COLUMN_NAME, place.Name)
+        values.put(PlaceEntry.COLUMN_ADDRESS, place.Address)
+        values.put(PlaceEntry.COLUMN_CATEGORY, place.Category)
+        DB.insert(PlaceEntry.TABLE_NAME, null, values)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
