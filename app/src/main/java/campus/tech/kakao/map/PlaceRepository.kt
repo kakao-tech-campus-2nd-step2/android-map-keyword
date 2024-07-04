1b0f91f1cd603c40f90cf82589ca2a3fda38c690
aengzu <jeon7879@naver.com>
2024-07-05 02:18:49 +0900

    feat(repo): add getPlaces method to PlaceRepository

--- app/src/main/java/campus/tech/kakao/map/PlaceRepository.kt
+++ app/src/main/java/campus/tech/kakao/map/PlaceRepository.kt
@@ -2,6 +2,7 @@ package campus.tech.kakao.map
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 
 class PlaceRepository(context: Context) {
     private val dbHelper = PlaceDBHelper.getInstance(context)
@@ -14,6 +15,32 @@ class PlaceRepository(context: Context) {
             put(PlaceDBContract.PlaceEntry.COLUMN_TYPE, place.type)
         }
         db.insert(PlaceDBContract.PlaceEntry.TABLE_NAME, null, values)
+        db.close()
+    }
+
+    fun getPlaces() {
+        val db = dbHelper.readableDatabase
+        val cursor: Cursor = db.query(
+            PlaceDBContract.PlaceEntry.TABLE_NAME,
+            arrayOf(
+                PlaceDBContract.PlaceEntry.COLUMN_NAME,
+                PlaceDBContract.PlaceEntry.COLUMN_ADDRESS,
+                PlaceDBContract.PlaceEntry.COLUMN_TYPE
+            ),
+            null, null, null, null, null
+        )
+
+        val places = mutableListOf<Place>()
+        with(cursor) {
+            while (moveToNext()) {
+                val name = getString(getColumnIndexOrThrow(PlaceDBContract.PlaceEntry.COLUMN_NAME))
+                val address =
+                    getString(getColumnIndexOrThrow(PlaceDBContract.PlaceEntry.COLUMN_ADDRESS))
+                val type = getString(getColumnIndexOrThrow(PlaceDBContract.PlaceEntry.COLUMN_TYPE))
+                places.add(Place(name, address, type))
+            }
+            close()
+        }
         db.close()
     }
 }
