5013e50c284e465a4826a4fc9a16ae23eaffbd2f
Lorenzo <tnalsdlekgsk1@icloud.com>
2024-07-05 00:14:37 +0900

    feat(MainActivity): TextWatcher 추가(추가만 해둠)

--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -2,6 +2,8 @@ package campus.tech.kakao.map
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.LinearLayout
@@ -9,6 +11,7 @@ import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import campus.tech.kakao.map.databinding.ActivityMainBinding
+import campus.tech.kakao.map.databinding.SearchHistoryItemBinding
 
 class MainActivity : AppCompatActivity() {
     private val viewModel : MainViewModel by viewModels()
@@ -20,21 +23,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
 
         val placeItems = viewModel.getPlace()
 
         if (placeItems == null) {
-            binding.emptyMainText.visibility = View.VISIBLE
+            mainBinding.emptyMainText.visibility = View.VISIBLE
         } else {
             placeAdapter = PlaceAdapter(placeItems)
-            binding.placeResult.adapter = placeAdapter
+            mainBinding.placeResult.adapter = placeAdapter
         }
 
+        val itemBinding = SearchHistoryItemBinding.inflate(layoutInflater, mainBinding.searchHistory, false)
+        itemBinding.history.text = ""
         val layoutInflater = this.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val customLayout = layoutInflater.inflate(R.layout.search_history_item, null)
         var textView: TextView = customLayout.findViewById<TextView>(R.id.history)
-        binding.searchHistory.addView(customLayout)
+        mainBinding.searchHistory.addView(customLayout)
+
+        val searchEditText = mainBinding.search
+        searchEditText.addTextChangedListener(object: TextWatcher
+        {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        }
+        )
     }
 }
