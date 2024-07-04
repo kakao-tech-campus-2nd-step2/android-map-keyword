c83ae04ed13bffbd941d9c36580ad25e6a90afab
이지은 <jieun7056@naver.com>
2024-07-04 22:42:22 +0900

    feat: 검색 기능 구현
    - searchEditText의 text에 따라 검색 결과 목록 띄우기

---
+++ app/src/main/java/campus/tech/kakao/map/LocationViewModel.kt
@@ -0,0 +1,27 @@
+package campus.tech.kakao.map
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LocationViewModel : ViewModel() {
+    private val _locations = MutableLiveData<List<Location>>()
+    val locations: LiveData<List<Location>> get() = _locations
+
+    private val _filteredLocations = MutableLiveData<List<Location>>()
+    val filteredLocations: LiveData<List<Location>> get() = _filteredLocations
+
+    fun setLocations(locations: List<Location>) {
+        _locations.value = locations
+        _filteredLocations.value = locations
+    }
+
+    fun filterLocations(query: String) {
+        val filteredList = _locations.value?.filter {
+            it.title.contains(query, ignoreCase = true) ||
+                    it.address.contains(query, ignoreCase = true) ||
+                    it.category.contains(query, ignoreCase = true)
+        }
+        _filteredLocations.value = filteredList
+    }
+}
--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -1,34 +1,71 @@
 package campus.tech.kakao.map
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var locationViewModel: LocationViewModel
+    private lateinit var locationAdapter: LocationAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dbAccessor = LocationDbAccessor(context=this)
-//        addLocationData(dbAccessor)
+        locationViewModel = ViewModelProvider(this).get(LocationViewModel::class.java)
+
+        val dbAccessor = LocationDbAccessor(context = this)
         val xButton: ImageView = findViewById(R.id.xButton)
         val searchEditText: EditText = findViewById(R.id.searchEditText)
         val searchResultRecyclerView: RecyclerView = findViewById(R.id.searchResultRecyclerView)
-        xButton.setOnClickListener{
-            searchEditText.setText("")
-        }
+
+        setupRecyclerView(searchResultRecyclerView)
+        setupSearchEditText(searchEditText)
+        setupXButton(xButton, searchEditText)
 
         val locationList: MutableList<Location> = readLocationData(dbAccessor)
-        val adapter: LocationAdapter = LocationAdapter()
-        searchResultRecyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
-        searchResultRecyclerView.adapter = adapter
+        locationViewModel.setLocations(locationList)
+        observeFilteredLocations()
 
-        adapter.submitList(locationList)
+        locationAdapter.submitList(locationList)
+    }
 
+    private fun setupRecyclerView(recyclerView: RecyclerView) {
+        locationAdapter = LocationAdapter()
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = locationAdapter
+    }
+
+    private fun setupSearchEditText(searchEditText: EditText) {
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                locationViewModel.filterLocations(s.toString())
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+
+    private fun setupXButton(xButton: ImageView, searchEditText: EditText) {
+        xButton.setOnClickListener {
+            searchEditText.setText("")
+        }
+    }
+
+    private fun observeFilteredLocations() {
+        locationViewModel.filteredLocations.observe(this, { locations ->
+            locationAdapter.submitList(locations)
+        })
     }
 
     private fun readLocationData(dbAccessor: LocationDbAccessor): MutableList<Location> {
@@ -45,5 +82,4 @@ class MainActivity : AppCompatActivity() {
             dbAccessor.insertLocation("음식점$i", "부산 부산진구 중앙대로$i", "음식점")
         }
     }
-
 }
