24658a3a59b3abc7d78d117a57c1186891ab5e71
aengzu <jeon7879@naver.com>
2024-07-04 20:04:16 +0900

    feat(repo): add insertPlace method to PlaceRepository

--- app/src/main/java/campus/tech/kakao/map/PlaceRepository.kt
+++ app/src/main/java/campus/tech/kakao/map/PlaceRepository.kt
@@ -1,4 +1,19 @@
 package campus.tech.kakao.map
 
-class PlaceRepository {
+import android.content.ContentValues
+import android.content.Context
+
+class PlaceRepository(context: Context) {
+    private val dbHelper = PlaceDBHelper.getInstance(context)
+
+    fun insertPlace(place: Place) {
+        val db = dbHelper.writableDatabase
+        val values = ContentValues().apply {
+            put(PlaceDBContract.PlaceEntry.COLUMN_NAME, place.name)
+            put(PlaceDBContract.PlaceEntry.COLUMN_ADDRESS, place.address)
+            put(PlaceDBContract.PlaceEntry.COLUMN_TYPE, place.type)
+        }
+        db.insert(PlaceDBContract.PlaceEntry.TABLE_NAME, null, values)
+        db.close()
+    }
 }
