3980334edbfff5929a582f17f97f559965c71f88
Lorenzo <tnalsdlekgsk1@icloud.com>
2024-07-04 20:17:59 +0900

    feat(MainActivity): 검색기록 뷰 연결

--- app/src/main/java/campus/tech/kakao/map/HistoryAdapter.kt
+++ app/src/main/java/campus/tech/kakao/map/HistoryAdapter.kt
@@ -1,4 +1,43 @@
 package campus.tech.kakao.map
 
-class HistoryAdapter() {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+
+class HistoryAdapter(private val items: List<String>): BaseAdapter() {
+    override fun getCount(): Int {
+        return items.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return items[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View
+        val placeViewHolder : PlaceViewHolder
+        if (convertView == null) {
+            view =
+                LayoutInflater.from(parent?.context).inflate(R.layout.search_history_item, parent, false)
+            placeViewHolder = PlaceViewHolder(view)
+            view.tag = placeViewHolder
+        } else {
+            view = convertView
+            placeViewHolder = convertView.tag as PlaceViewHolder
+        }
+
+        val item = items[position]
+        placeViewHolder.history.text = item
+        return view
+    }
+
+    class PlaceViewHolder(view: View) {
+        val history: TextView = view.findViewById(R.id.history)
+    }
 }
--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -1,7 +1,11 @@
 package campus.tech.kakao.map
 
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import campus.tech.kakao.map.databinding.ActivityMainBinding
@@ -9,6 +13,8 @@ import campus.tech.kakao.map.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private val viewModel : MainViewModel by viewModels()
     private lateinit var placeAdapter: PlaceAdapter
+    private lateinit var historyAdapter: HistoryAdapter
+    private lateinit var container: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,13 +23,18 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val items = viewModel.getPlace()
+        val placeItems = viewModel.getPlace()
 
-        if (items == null) {
+        if (placeItems == null) {
             binding.emptyMainText.visibility = View.VISIBLE
         } else {
-            placeAdapter = PlaceAdapter(items)
+            placeAdapter = PlaceAdapter(placeItems)
             binding.placeResult.adapter = placeAdapter
         }
+
+        val layoutInflater = this.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val customLayout = layoutInflater.inflate(R.layout.search_history_item, null)
+        var textView: TextView = customLayout.findViewById<TextView>(R.id.history)
+        binding.searchHistory.addView(customLayout)
     }
 }
