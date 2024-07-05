96926be6d7d609ba3b54afa73666376c5f9a9362
jmc98 <jmc0820@naver.com>
2024-07-05 17:41:31 +0900

    Feat: 생성 addPlace
    장소 추가 함수

--- app/src/main/java/campus/tech/kakao/map/PlaceDbHelper.kt
+++ app/src/main/java/campus/tech/kakao/map/PlaceDbHelper.kt
@@ -1,5 +1,6 @@
 package campus.tech.kakao.map
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
@@ -8,8 +9,7 @@ class PlaceDbHelper(context: Context):SQLiteOpenHelper(
 	context, "place.db", null, 1) {
 	override fun onCreate(db: SQLiteDatabase?) {
 		db?.execSQL("CREATE TABLE ${PlaceContract.TABLE_NAME} " +
-				"(id INTEGER PRIMARY KEY, " +
-				"${PlaceContract.COLUMN_NAME_NAME} TEXT, " +
+				"(${PlaceContract.COLUMN_NAME_NAME} TEXT, " +
 				"${PlaceContract.COLUMN_NAME_ADDRESS} TEXT, " +
 				"${PlaceContract.COLUMN_NAME_TYPE} TEXT)")
 	}
@@ -17,5 +17,15 @@ class PlaceDbHelper(context: Context):SQLiteOpenHelper(
 	override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
 		db?.execSQL("DROP TABLE IF EXISTS ${PlaceContract.TABLE_NAME}")
 		onCreate(db)
+	}
+
+	fun addPlace(place: Place) {
+		val db = writableDatabase
+		val values = ContentValues()
+		values.put(PlaceContract.COLUMN_NAME_NAME, place.name)
+		values.put(PlaceContract.COLUMN_NAME_ADDRESS, place.address)
+		values.put(PlaceContract.COLUMN_NAME_TYPE, place.type)
+		db.insert(PlaceContract.TABLE_NAME, null, values)
+		db.close()
 	}
 }
