85f95a650f010ca0c946b562e0ca7ffc11084a81
aengzu <jeon7879@naver.com>
2024-07-05 14:31:30 +0900

    feat: Bind views using findViewById in PlaceActivity

--- app/src/main/java/campus/tech/kakao/map/PlaceActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/PlaceActivity.kt
@@ -2,15 +2,22 @@ package campus.tech.kakao.map
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 
 class PlaceActivity : AppCompatActivity() {
     private lateinit var placeViewModel: PlaceViewModel
     private lateinit var placeViewModelFactory: PlaceViewModelFactory
+    val searchEditText = findViewById<EditText>(R.id.searchEditText)
+    val removeButton = findViewById<ImageButton>(R.id.cancelButton)
+    val placeRecyclerView = findViewById<RecyclerView>(R.id.placeRecyclerView)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_place)
