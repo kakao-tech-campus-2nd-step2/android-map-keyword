4f9066a96749365402a6cfa23f8112d127b87f87
jmc98 <jmc0820@naver.com>
2024-07-05 14:43:24 +0900

    Feat: 생성 데이터베이스

--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -1,11 +1,43 @@
 package campus.tech.kakao.map
 
+import android.content.ContentValues
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = PlaceDbHelper(this)
+        val writableDatabase = db.writableDatabase
+        val values = ContentValues()
+        values.put(PlaceContract.COLUMN_NAME_NAME, "카페1")
+        values.put(PlaceContract.COLUMN_NAME_ADDRESS, "서울시 강남구")
+
+        writableDatabase.insert(PlaceContract.TABLE_NAME, null, values)
+
+        val readableDatabase = db.readableDatabase
+        var searchResult = "%카페%"
+        val cursor = readableDatabase.query(
+            PlaceContract.TABLE_NAME,
+            arrayOf(PlaceContract.COLUMN_NAME_NAME),
+            "${PlaceContract.COLUMN_NAME_NAME} like ?",
+            arrayOf(searchResult),
+            null,
+            null,
+            "${PlaceContract.COLUMN_NAME_NAME} DESC"
+        )
+
+        val result = mutableListOf<String>()
+        while (cursor.moveToNext()) {
+            result.add(
+                cursor.getString(
+                    cursor.getColumnIndexOrThrow(PlaceContract.COLUMN_NAME_NAME)
+                )
+            )
+        }
+        cursor.close()
     }
 }
---
+++ app/src/main/java/campus/tech/kakao/map/PlaceDbHelper.kt
@@ -0,0 +1,21 @@
+package campus.tech.kakao.map
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class PlaceDbHelper(context: Context):SQLiteOpenHelper(
+	context, "place.db", null, 1) {
+	override fun onCreate(db: SQLiteDatabase?) {
+		db?.execSQL("CREATE TABLE ${PlaceContract.TABLE_NAME} " +
+				"(id INTEGER PRIMARY KEY, " +
+				"${PlaceContract.COLUMN_NAME_NAME} TEXT, " +
+				"${PlaceContract.COLUMN_NAME_ADDRESS} TEXT, " +
+				"${PlaceContract.COLUMN_NAME_TYPE} TEXT)")
+	}
+
+	override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+		db?.execSQL("DROP TABLE IF EXISTS ${PlaceContract.TABLE_NAME}")
+		onCreate(db)
+	}
+}
