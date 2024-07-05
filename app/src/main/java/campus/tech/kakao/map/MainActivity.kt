c96d1d448a58e4c2bb51c5706c86af0e614ef9c1
Dong-Han <donghk77@naver.com>
2024-07-05 19:59:54 +0900

    [Feat] #4 - 검색 데이터 SQLite 저장

--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -2,10 +2,27 @@ package campus.tech.kakao.map
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import campus.tech.kakao.map.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var sqliteInit: SQLiteInit
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize SQLite database
+        sqliteInit = SQLiteInit(this)
+
+        // Now you can launch coroutines from here
+        lifecycleScope.launch(Dispatchers.IO) {
+            sqliteInit.insertSampleData()
+        }
     }
 }
