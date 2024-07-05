3397baf078860460c663a655d0fa05876e7b8468
aengzu <jeon7879@naver.com>
2024-07-05 14:04:23 +0900

    feat(vm): Implement ViewModel, ViewModelFactory, and dummy data insertion
    - PlaceViewModel 에 context 를 제공하기 위해 PlaceViewModelFactory 구현
    - repository 와 상호작용하는 PlaceViewModel 구현
    - PlaceRepository 내에 더미 데이터를 데이터베이스에 삽입하는 로직 추가
    - PlaceActivity 에서 PlaceViewModelFactory를 통해 PlaceViewModel 인스턴스 생성

--- app/src/main/java/campus/tech/kakao/map/PlaceRepository.kt
+++ app/src/main/java/campus/tech/kakao/map/PlaceRepository.kt
@@ -7,6 +7,10 @@ import android.database.Cursor
 class PlaceRepository(context: Context) {
     private val dbHelper = PlaceDBHelper.getInstance(context)
 
+    init {
+        insertDummies()
+    }
+
     fun insertPlace(place: Place) {
         val db = dbHelper.writableDatabase
         val values = ContentValues().apply {
@@ -15,10 +19,10 @@ class PlaceRepository(context: Context) {
             put(PlaceDBContract.PlaceEntry.COLUMN_TYPE, place.type)
         }
         db.insert(PlaceDBContract.PlaceEntry.TABLE_NAME, null, values)
-        db.close()
+       // db.close()
     }
 
-    fun getPlaces() {
+    fun getPlaces(): MutableList<Place> {
         val db = dbHelper.readableDatabase
         val cursor: Cursor = db.query(
             PlaceDBContract.PlaceEntry.TABLE_NAME,
@@ -40,6 +44,39 @@ class PlaceRepository(context: Context) {
                 places.add(Place(name, address, type))
             }
             close()
+        }
+        // db.close()
+        return places
+    }
+
+    fun insertDummies() {
+        clearPlaces()
+
+        val db = dbHelper.writableDatabase
+        val dummyPlaces = ((1..30).map {
+            Place("카페 $it", "대전 서구 만년동 $it", "카페")
+        })
+        db.beginTransaction()
+        for (place in dummyPlaces) {
+            val value = ContentValues().apply {
+                put(PlaceDBContract.PlaceEntry.COLUMN_NAME, place.name)
+                put(PlaceDBContract.PlaceEntry.COLUMN_ADDRESS, place.address)
+                put(PlaceDBContract.PlaceEntry.COLUMN_TYPE, place.type)
+            }
+            db.insert(PlaceDBContract.PlaceEntry.TABLE_NAME, null, value)
+        }
+        db.setTransactionSuccessful()
+        db.endTransaction()
+    }
+
+    private fun clearPlaces() {
+        val db = dbHelper.writableDatabase
+        db.beginTransaction()
+        try {
+            db.delete(PlaceDBContract.PlaceEntry.TABLE_NAME, null, null)
+            db.setTransactionSuccessful()
+        } finally {
+            db.endTransaction()
         }
         db.close()
     }
--- app/src/main/java/campus/tech/kakao/map/PlaceViewModel.kt
+++ app/src/main/java/campus/tech/kakao/map/PlaceViewModel.kt
@@ -1,4 +1,34 @@
 package campus.tech.kakao.map
 
-class PlaceViewModel {
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class PlaceViewModel(private val context: Context) : ViewModel() {
+    private val repository: PlaceRepository = PlaceRepository(context)
+    private val _places = MutableLiveData<List<Place>>()
+    val places: LiveData<List<Place>> get() = _places
+
+    init {
+        loadPlaces()
+    }
+
+    private fun loadPlaces() {
+        val loadedPlaces = repository.getPlaces()
+        _places.postValue(loadedPlaces)
+    }
+
+    fun insertPlace(place: Place) {
+        repository.insertPlace(place)
+        loadPlaces()
+    }
+
+    fun getPlaces() {
+        loadPlaces()
+    }
+
+
 }
+
+
