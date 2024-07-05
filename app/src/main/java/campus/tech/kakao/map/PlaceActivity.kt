869777fc2b86d82e081a4f0a0e18dc134b88dc44
aengzu <jeon7879@naver.com>
2024-07-05 16:44:23 +0900

    feat: add remove searchEditText functionality

--- app/src/main/java/campus/tech/kakao/map/PlaceActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/PlaceActivity.kt
@@ -6,6 +6,7 @@ import android.text.TextWatcher
 import android.util.Log
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,37 +21,62 @@ class PlaceActivity : AppCompatActivity() {
     private lateinit var placeViewModelFactory: PlaceViewModelFactory
     private lateinit var searchEditText: EditText
     private lateinit var removeButton: ImageButton
+    private lateinit var emptyMessage: TextView
     private lateinit var placeRecyclerView: RecyclerView
     private lateinit var placeAdapter: PlaceAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_place)
         placeViewModelFactory = PlaceViewModelFactory(this)
-        placeViewModel = ViewModelProvider(this, placeViewModelFactory).get(PlaceViewModel::class.java)
+        placeViewModel =
+            ViewModelProvider(this, placeViewModelFactory).get(PlaceViewModel::class.java)
 
         searchEditText = findViewById(R.id.searchEditText)
         removeButton = findViewById(R.id.cancelButton)
         placeRecyclerView = findViewById(R.id.placeRecyclerView)
+        emptyMessage = findViewById(R.id.emptyMessage)
         placeAdapter = PlaceAdapter()
 
         placeRecyclerView.layoutManager = LinearLayoutManager(this)
         placeRecyclerView.adapter = placeAdapter
 
-        placeViewModel.places.observe(this, { places ->
-            placeAdapter.updateData(places)
-        })
+        placeViewModel.places.observe(this) { places ->
+            if (places.isEmpty()) {
+                showEmptyMessage()
+            } else {
+                emptyMessage.visibility = TextView.GONE
+                placeRecyclerView.visibility = RecyclerView.VISIBLE
+                placeAdapter.updateData(places)
+            }
+        }
 
         searchEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 //
             }
+
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 placeViewModel.searchPlaces(p0.toString())
-                // LiveData 업데이트 후 RecyclerView에 반영
             }
+
             override fun afterTextChanged(p0: Editable?) {
                 //
             }
         })
+
+        removeButton.setOnClickListener {
+            clearSearchEditText()
+            showEmptyMessage()
+        }
+    }
+
+
+    private fun showEmptyMessage() {
+        emptyMessage.visibility = TextView.VISIBLE
+        placeRecyclerView.visibility = RecyclerView.GONE
+    }
+
+    private fun clearSearchEditText() {
+        searchEditText.text.clear()
     }
 }
