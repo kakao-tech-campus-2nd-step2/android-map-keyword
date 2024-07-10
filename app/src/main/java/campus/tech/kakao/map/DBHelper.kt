2d97d9463a7c3f8915355a320e8b0fb315305ca8
khyeonm <101093716+khyeonm@users.noreply.github.com>
2024-07-11 02:13:15 +0900

    부산대 Android_김현민 2주차 과제 Step2 (#42)
    * Docs: Update README.md
    
    * Feat: Complete search screen layout
    
    * Feat: Add data to the database
    
    * Docs: Update README.md
    
    * Feat: Create item_view
    
    * Feat: Modify main view
    
    * Feat: Implement search list
    
    * Feat: Complete saved element view
    
    * Feat: Complete save action
    
    * Feat: Add persistent save function
    
    * Design: Modify save element design
    
    * Update README.md
    
    * Fix: Resolve error

--- app/src/main/java/campus/tech/kakao/map/DBHelper.kt
+++ app/src/main/java/campus/tech/kakao/map/DBHelper.kt
@@ -1,30 +1,27 @@
 package campus.tech.kakao.map
 
-import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import android.os.Parcel
-import android.os.Parcelable
 
 private const val TABLE_PLACE = "places"
-private const val COLUMN_TYPE = "type"  //
+private const val COLUMN_TYPE = "type"
 private const val COLUMN_NAME = "name"
 private const val COLUMN_ADDRESS = "address"
 
-class DBHelper(context: Context): SQLiteOpenHelper(context, "place.db", null, 1) {
+class DBHelper(context: Context) : SQLiteOpenHelper(context, "place.db", null, 1) {
     override fun onCreate(db: SQLiteDatabase?) {
         db?.execSQL(
             "CREATE TABLE $TABLE_PLACE (" +
-                    "$COLUMN_TYPE  varchar(30) NOT NULL," +
-                    "$COLUMN_NAME  varchar(30) NOT NULL," +
-                    "$COLUMN_ADDRESS  varchar(30) NOT NULL" +
+                    "$COLUMN_TYPE VARCHAR(30) NOT NULL," +
+                    "$COLUMN_NAME VARCHAR(30) NOT NULL," +
+                    "$COLUMN_ADDRESS VARCHAR(30) NOT NULL" +
                     ");"
         )
 
-        insertPharData()
-        insertCafeData()
+        insertPharData(db)
+        insertCafeData(db)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
@@ -32,30 +29,48 @@ class DBHelper(context: Context): SQLiteOpenHelper(context, "place.db", null, 1)
         onCreate(db)
     }
 
-    fun insertPharData() {
-        val db = this.writableDatabase
+    private fun insertPharData(db: SQLiteDatabase?) {
         val type = "약국"
         val name = "약국"
         val address = "서울 강남구 대치동"
 
         for (i in 1..30) {
-            val name = "$name$i"
-            val address = "$address $i"
-            db.execSQL("INSERT INTO $TABLE_PLACE ($COLUMN_TYPE, $COLUMN_NAME, $COLUMN_ADDRESS) VALUES ('$type', '$name', '$address');")
+            val nameWithIndex = "$name$i"
+            val addressWithIndex = "$address $i"
+            db?.execSQL("INSERT INTO $TABLE_PLACE ($COLUMN_TYPE, $COLUMN_NAME, $COLUMN_ADDRESS) VALUES ('$type', '$nameWithIndex', '$addressWithIndex');")
         }
     }
 
-    fun insertCafeData() {
-        val db = this.writableDatabase
+    private fun insertCafeData(db: SQLiteDatabase?) {
         val type = "카페"
         val name = "카페"
         val address = "서울 성동구 성수동"
 
         for (i in 1..30) {
-            val name = "$name$i"
-            val address = "$address $i"
-            db.execSQL("INSERT INTO $TABLE_PLACE ($COLUMN_TYPE, $COLUMN_NAME, $COLUMN_ADDRESS) VALUES ('$type', '$name', '$address');")
+            val nameWithIndex = "$name$i"
+            val addressWithIndex = "$address $i"
+            db?.execSQL("INSERT INTO $TABLE_PLACE ($COLUMN_TYPE, $COLUMN_NAME, $COLUMN_ADDRESS) VALUES ('$type', '$nameWithIndex', '$addressWithIndex');")
         }
+    }
+
+    fun searchProfiles(query: String): List<Profile> {
+        val profiles = mutableListOf<Profile>()
+        val db = this.readableDatabase
+        val cursor: Cursor = db.rawQuery(
+            "SELECT * FROM $TABLE_PLACE WHERE $COLUMN_NAME LIKE ? OR $COLUMN_ADDRESS LIKE ? OR $COLUMN_TYPE LIKE ?",
+            arrayOf("%$query%", "%$query%", "%$query%")
+        )
+
+        if (cursor.moveToFirst()) {
+            do {
+                val type = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_TYPE))
+                val name = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_NAME))
+                val address = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_ADDRESS))
+                profiles.add(Profile(name, address, type))
+            } while (cursor.moveToNext())
+        }
+        cursor.close()
+        return profiles
     }
 }
 
