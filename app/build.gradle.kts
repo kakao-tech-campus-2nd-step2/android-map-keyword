92728843bbd1b5df7a1cf53d5f055e15bbd45de2
Dong-Han <donghk77@naver.com>
2024-07-10 15:46:57 +0900

    Chore : gradle implementation(viewmodels) 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,11 @@ android {
 
 dependencies {
 
+    // viewmodels 추가
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+    implementation ("androidx.activity:activity-ktx:1.4.0")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
