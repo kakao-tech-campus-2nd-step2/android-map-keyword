f8bacfb3e50798aa119317666bb059ac84bfa455
Aslop <122802059+Pjhn@users.noreply.github.com>
2024-07-11 02:12:45 +0900

    부산대Android_박정훈_2주차_과제_Step2  (#62)
    * docs: README.md 작성
    
    * design: 기본 화면 생성
    
    * design: 리스트 뷰 아이템 요소 디자인
    
    * feat: 위치 정보를 출력하는 리스트 뷰 구현
    
    - LocationListViewAdapter() 리스트뷰 어댑터 구현
    - Location() 지역 정보를 담을 데이터 클래스 구현
    - 더미 데이터 생성 후 테스트
    
    * feat: 데이터베이스 구축
    
    - 데이터베이스 테이블 이름, 칼럼(이름, 위치, 타입) 상수화
    - SQLiteOpenHelper 객체 구현
    - 간단한 데이터 입력 테스트 수행
    
    * refactor: MVVM 패턴 적용
    
    - MVVM 패턴 적용 시도
    - ViewModel 싱글톤 구현
    
    Remaining issues:
    - 옵저버에서 데이터 베이스의 변화를 1번만 감지하는 문제 발생
    
    * refactor: 리스트 뷰 더미 생성 코드 삭제
    
    * feat: README.md 수정
    
    - step2 기능 요구 사항 작성
    
    * design: 검색 기록 UI 추가
    
    * refactor: MVVM 패턴 구현 파일을 패키지로 분류
    
    * feat: 실시간 검색 기능 추가
    
    - 검색어가 바뀔 때마다 위치정보 객체 전송
    
    * remove: ListView 어댑터 파일 제거
    
    * feat: 검색 내용에 따라 검색결과 목록 표시 기능 추가
    
    - 검색 결과 목록 RecyclerView 어댑터 추가
    
    * feat: 안내 메세지 숨김 기능 추가
    
    - 검색 목록에 데이터 있을 때 안내 메세지 숨기기
    
    * feat: 선택된 항목 검색어 저장 목록에 추가
    
    - 저장된 검색어 목록은 가로 스크롤이 된다
    - 저장된 검색어는 x를 눌러서 삭제할 수 있다
    
    * refactor: 가독성 개선을 위한 코드 리팩토링
    
    - 기능 별 함수 분리
    
    * feat: 입력한 검색어 삭제 기능 추가
    
    * feat: 검색 기록 저장 기능 추가
    
    * refactor: ViewModelProvider를 활용한 ViewModel 객체 생성
    
    - LocationViewModelFactory 구현
    
    * fix: 검색 결과가 있을 때 도움말 메시지가 사라지지 않는 버그 수정
    
    * refactor: ViewActivity 가독성 개선
    
    - ViewActivity 함수 분리 및 변수명 변경
    
    * refactor: LocationAdapter에 DiffUtil 적용
    
    - DiffUtilcallback 클래스 추가
    
    * refactor: LogAdapter에 DiffUtil 적용
    
    * refactor: RecyclerView 어댑터를 ListAdapter를 사용하도록 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -36,6 +37,7 @@ android {
 
     buildFeatures {
         viewBinding = true
+        dataBinding = true
     }
 }
 
@@ -47,6 +49,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.activity:activity:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
